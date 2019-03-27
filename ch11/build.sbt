76b395fae09e0fa7a0144d2a5637b66e67076dcb
todd-ddd <todd.ddd@kakaocorp.com>
2019-03-27 18:37:56 +0900

    init

---
+++ ch11/build.sbt
@@ -0,0 +1,5 @@
+name := "ch11"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
---
+++ ch11/src/main/scala/Problem1.scala
@@ -0,0 +1,16 @@
+trait Functor[F[_]] {
+  def map[A, B](fa: F[A])(f: A => B): F[B]
+}
+
+trait Monad[F[_]] extends Functor[F] {
+  def unit[A](a: => A): F[A]
+  def flatMap[A, B](ma: F[A])(f: A => F[B]): F[B]
+  def map[A, B](ma: F[A])(f: A => B): F[B] =
+    flatMap(ma)(a => unit(f(a)))
+  def map2[A, B, C](ma: F[A], mb: F[B])(f: (A, B) => C): F[C] =
+    flatMap(ma)(a => map(mb)(b => f(a, b)))
+}
+
+object Problem1 extends App {
+
+}
